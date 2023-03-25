bde41ac83bf2702a2865a96f00b2c43560ee1c85
Bogdan Titsky <bogdan.titsky@gmail.com>
2023-03-25 22:08:33 +0200

    script

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,13 +1,22 @@
-if (window.innerWidth >= 768) {
-  function openNav() {
-    document.getElementById('sidebar').style.width = '290px';
-  }
-} else {
-  function openNav() {
-    document.getElementById('sidebar').style.width = '250px';
-  }
-}
+window.addEventListener('DOMContentLoaded', event => {
+  const sidebarElement = document.getElementById('sidebar');
+  const openBtnElement = document.querySelector('.btn-open-menu');
+  const closeBtnElement = document.querySelector('.btn-close-menu');
+  const menuLinks = document.querySelectorAll('.mobile-menu-list-link');
+  menuLinks.forEach(item => {
+    item.addEventListener('click', () => {
+      sidebarElement.style.width = '0';
+    });
+  });
+  openBtnElement.addEventListener('click', () => {
+    if (window.innerWidth >= 768) {
+      sidebarElement.style.width = '290px';
+    } else {
+      sidebarElement.style.width = '250px';
+    }
+  });
 
-function closeNav() {
-  document.getElementById('sidebar').style.width = '0';
-}
+  closeBtnElement.addEventListener('click', () => {
+    sidebarElement.style.width = '0';
+  });
+});
