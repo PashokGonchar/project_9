457b78d3439ee89a480da5ad5666768498d4362d
Kishyk <123320655+Kishyk@users.noreply.github.com>
2023-03-26 23:19:55 +0300

    Add modal (html, css, js)

--- src/js/modal.js
+++ src/js/modal.js
@@ -13,3 +13,26 @@
   }
 })();
 
+
+/**
+ * reviews section
+ */
+(() => {
+  const refsReviews = {
+    openModalBtn: document.querySelector('.reviews-button'),
+    closeModalBtn: document.querySelector('.reviews-modal-close-button'),
+    modal: document.querySelector('.reviews-modal-wrapper'),
+  };
+
+  refsReviews.openModalBtn.addEventListener('click', toggleModal);
+  refsReviews.closeModalBtn.addEventListener('click', toggleModal);
+
+  let isOpen = false;
+
+  function toggleModal() {
+    document.body.style.overflow = isOpen ? null : `hidden`;
+    refsReviews.modal.classList.toggle('reviews-modal-wrapper-hidden');
+    isOpen = !isOpen;
+  }
+})();
+
