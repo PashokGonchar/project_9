afec13765d3c2e69052a84c0e093509ecaa2cacb
AnastasiiaKhranovska <123419111+AnastasiiaKhranovska@users.noreply.github.com>
2023-03-28 16:22:20 -0500

    ready

--- src/js/slider.js
+++ src/js/slider.js
@@ -60,3 +60,25 @@ new Swiper('.hero-swiper', {
     dynamicMainBullets: 3,
   },
 });
+
+
+new Swiper('.reviews-swiper', {
+  slidesPerView: 1,
+  spaceBetween: 16,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 28,
+    },
+  },
+  pagination: {
+    el: '.reviews-swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+    dynamicMainBullets: 2.5,
+  },
+});
